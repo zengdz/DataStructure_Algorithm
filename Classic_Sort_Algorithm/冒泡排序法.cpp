94c0cbd567d7d2699bd3fffb7fa69ef56aff50f2
zengdz <zeng.zhen@foxmail.com>
2017-06-14 11:04:47 +0800

    冒泡排序法(更新)
    完善不同类型冒泡排序，添加理解的注释

--- Classic_Sort_Algorithm/冒泡排序法.cpp
+++ Classic_Sort_Algorithm/冒泡排序法.cpp
@@ -1,5 +1,7 @@
 ﻿/*
- * 冒泡排序法
+ * 冒泡排序法(默认递增排序，可以修改交换条件来改变排序方式)
+ * 还有两种冒泡形式：冒小泡还是冒大泡
+ * 在写代码时，画出索引的位置分布以及理解索引含义可理清思路
  */
 
 #include <iostream>
@@ -13,21 +15,50 @@ void SelfSwap(int &a, int &b)
 	b = temp;
 }
 
-void BubbleSort(int *a, int n)
+// 冒泡排序-冒小泡 从后往前冒泡
+void BubbleSortSmall(int *a, int n)
 {
-	// flag用于判断是否发生交换，如果没有交换说明已经有序不用处理
+	// flag指示是否发生交换，如果没有交换说明已经有序不用处理
 	bool flag = true;
+	// 索引i代表循环结束位置i的元素已在有序位置
+	// 冒小泡所以i从0开始，不用取到最后一个位置，i不用等于n-1
+	// 因为其他有序剩下一个必然有序
 	for(int i = 0; i < n-1 && flag; ++i){
 		flag = false;
-		// j是从n-2开始递减,因为是倒数第2个和倒数第1个比较,而且判断条件要取到i的值
-		// 注意j的判断条件和i有关
+		// 索引j用于每轮冒泡的比较，由于是冒小泡，所以j从后面开始
+		// 用于比较的索引选择的是j和j+1，所以初值即上限j+1=n-1即j=n-2
+		// j的下限是i，目的就是把小泡冒泡到i位置
 		for(int j = n - 2; j >= i; --j){
+			// 递增排序的比较条件,若递减把比较改为小于即可
 			if(a[j] > a[j+1]){
 				SelfSwap(a[j], a[j+1]);
 				flag = true;
 			}
 		}
 	} 
+}
+
+// 冒泡排序-冒大泡 从前往后冒泡
+void BubbleSortBig(int *a, int n)
+{
+	// flag指示是否发生交换，如果没有交换说明已经有序不用处理
+	bool flag = true;
+	// 索引i代表循环结束位置i的元素已在有序位置
+	// 冒大泡所以i从n-1开始，不用取到第一个位置，i不用等于0
+	// 因为其他有序剩下一个必然有序
+	for (int i = n - 1; i > 0 && flag; --i) {
+		flag = false;
+		// 索引j用于每轮冒泡的比较，由于是冒大泡，所以j从前面开始
+		// 用于比较的索引选择的是j和j+1，所以初值下限j=0
+		// 上限是j+1=i，目的就是把大泡冒泡到i位置
+		for (int j = 0; j + 1 <= i; ++j) {
+			// 递增排序的比较条件,若递减把比较改为小于即可
+			if (a[j] > a[j + 1]) {
+				SelfSwap(a[j], a[j + 1]);
+				flag = true;
+			}
+		}
+	}
 }
 
 // 输出结果函数，带一个字符串参数用于输出提示信息
@@ -43,11 +74,11 @@ void PrintArrayResult(const char *s, const int arr[], const int length)
 
 int main()
 {
-	int a[] = {8,2,5,4,5,7,3,13,23,8,21,66,33};
-	int length = sizeof(a) / sizeof(a[0]);
+	int arr[] = {8,2,5,4,5,7,3,13,23,8,21,66,33};
+	int length = sizeof(arr) / sizeof(arr[0]);
 
-	BubbleSort(a, length);
-	PrintArrayResult("Bubble sort result:", a, length);
+	BubbleSortSmall(arr, length);
+	PrintArrayResult("Bubble sort result:", arr, length);
 
 	return 0;
 }
