25ffb2ddf04f43e508043c597055d9b3a6285a12
zengdz <zeng.zhen@foxmail.com>
2017-06-17 22:07:59 +0800

    链表基础操作(更新)

--- Linked_List/链表基础操作.cpp
+++ Linked_List/链表基础操作.cpp
@@ -10,6 +10,17 @@ struct Node {
 	struct Node *next;
 };
 
+void AddToHead(Node **pHead, int value)
+{
+	Node *pNew = new Node();
+	pNew->data = value;
+	pNew->next = nullptr;
+
+	// 无需单独处理链表为空的情况
+	pNew->next = *pHead;
+	*pHead = pNew;
+}
+
 // 在链表末尾添加一个节点
 // 注意形参是一个指向指针的指针，可以修改指针
 void AddToTail(Node **pHead, int value)
@@ -29,6 +40,26 @@ void AddToTail(Node **pHead, int value)
 		// 找到next成员为nullptr的节点即末尾，将其指向新的节点
 		pNode->next = pNew;
 	}
+}
+
+void DeleteNthNode(Node **pHead, int n)
+{
+	if (pHead == nullptr || n < 1)
+		return;
+	Node *pNode = *pHead;
+	if (n == 1) {
+		*pHead = pNode->next;
+		delete pNode;
+		return; // 不能少了return语句，因为后面有pNode的使用
+	}
+	// 为什么循环条件是n-2
+	// 目的是指向第n-1位置，因为pNode已经指向第一个位置
+	// 所以只需要循环n-2次即可
+	for (int i = 0; i < n - 2; ++i)
+		pNode = pNode->next; // pNode指向第n-1个节点
+	Node *temp = pNode->next; // temp保存第n个节点
+	pNode->next = temp->next; // pNode指向temp后一个节点
+	delete temp; // 删除第n个节点
 }
 
 // 删除链表中第一个值为value的节点
@@ -57,7 +88,7 @@ void RemoveNode(Node **pHead, int value)
 }
 
 // 打印输出链表
-void printList(struct Node *n)
+void printList(Node *n)
 {
 	while (n != nullptr) {
 		cout << n->data << " ";
@@ -66,18 +97,23 @@ void printList(struct Node *n)
 	cout << endl;
 }
 
-int main(){
+int main()
+{
+	// head只是一个指针，指向的地方是一个Node结构体
+	// 如果是空链表，头指针就是一个空指针
+	Node *head = nullptr;
 
-	Node *head = new Node;
-	head->data = 1;
-	head->next = nullptr;
-
+	AddToHead(&head, 1);
 	AddToTail(&head, 5);
 	AddToTail(&head, 13);
 	AddToTail(&head, 1);
+	AddToHead(&head, 7);
 	printList(head);
 
 	RemoveNode(&head, 1);
+	printList(head);
+
+	DeleteNthNode(&head, 1);
 	printList(head);
 
 	return 0;
