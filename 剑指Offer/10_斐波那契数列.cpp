b74d6e3e17311a8ee1597eedfc8e3be4b8bd30a1
zengdz <zeng.zhen@foxmail.com>
2017-08-31 17:08:52 +0800

    斐波那契(dp实战)
    之前的代码，其中的迭代法其实可以看作是空间优化

--- 剑指Offer/10_斐波那契数列.cpp
+++ 剑指Offer/10_斐波那契数列.cpp
@@ -1,62 +1,60 @@
-﻿#include <stdio.h>
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
 // 递归法：大量重复运算，使用大量栈内存，
 // 运算较慢，有栈溢出风险
-long Fibonacci_Recursive(int n)
-{
-	if (n <= 0)
-		return 0;
-	if (n == 1)
-		return 1;
-	return Fibonacci_Recursive(n - 1) + Fibonacci_Recursive(n - 2);
-}
-
-// 迭代法：根据上一次运算结果进行计算
-long Fibonacci_Iterative(int n)
-{
-	if (n <= 0)
-		return 0;
-	if (n == 1)
-		return 1;
-	int Fib_2 = 0;
-	int Fib_1 = 1;
-	int FibN = 0;
-	for (int i = 2; i <= n; ++i) {
-		FibN = Fib_1 + Fib_2; // 计算第N个斐波那契值
-		// 更新变量，注意顺序
-		Fib_2 = Fib_1; 
-		Fib_1 = FibN;
-	}
-	return FibN;
+long Fibonacci_Recursive(int n) {
+    if (n <= 0) // 递归的终止条件
+        return 0;
+    if (n == 1)
+        return 1;
+    return Fibonacci_Recursive(n - 1) + Fibonacci_Recursive(n - 2);
 }
 
 // 动态规划法：需要额外内存空间
-// 相比迭代法，更直观容易理解
-long Fibonacci_DP(int n)
-{
-	if (n <= 0)
-		return 0;
-	if (n == 1)
-		return 1;
-	int *fib = new int[n + 1]; // 第一个元素是0，总共n+1个
-	fib[0] = 0;
-	fib[1] = 1;
-	for (int i = 2; i <= n; ++i) {
-		// 根据数组保存的结果计算当前斐波那契值
-		fib[i] = fib[i - 2] + fib[i - 1];
-	}
-	return fib[n];
-	delete[] fib;
+long Fibonacci_DP(int n) {
+    // 虽然DP有每个位置的记录，不用初值判断
+    // 但DP只能处理正常值输入，最好还是加上
+    if (n <= 0) 
+        return 0; 
+    if (n == 1)
+        return 1;
+    vector<int> fib(n + 1); // 第一个元素是0，总共n+1个
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        // 根据数组保存的结果计算当前斐波那契值
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+    return fib[n];
+}
+
+// 空间优化：使用两个变量滚动计算
+long Fibonacci_SpaceCompress(int n) {
+    if (n <= 0) // 空间优化没有保存每个值，需要初值判断
+        return 0;
+    if (n == 1)
+        return 1;
+    int Fib_2 = 0;
+    int Fib_1 = 1;
+    int FibN = 0;
+    for (int i = 2; i <= n; ++i) {
+        FibN = Fib_1 + Fib_2; // 计算第N个斐波那契值
+        Fib_2 = Fib_1; // 更新变量，注意顺序
+        Fib_1 = FibN;
+    }
+    return FibN;
 }
 
 int main()
 {
-	cout << Fibonacci_Iterative(39) << endl;
-	cout << Fibonacci_DP(39) << endl;
-	cout << Fibonacci_Recursive(39) << endl;
-	
-	return 0;
+    int n;
+    cin >> n;
+    cout << Fibonacci_Recursive(n) << endl;
+    cout << Fibonacci_DP(n) << endl;
+    cout << Fibonacci_SpaceCompress(n) << endl;
+
+    return 0;
 }
